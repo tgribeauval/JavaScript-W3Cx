8656336240f734ea02d3c5f31aa02a9516643205
Thierry Gribeauval <t.gribeauval11@gmail.com>
2020-02-12 22:28:21 -0500

    Adding and moving elements

---
+++ DOMApi/MoveElements/MoveElem/js/index.js
@@ -0,0 +1,8 @@
+function move(elem) {
+  var targetList = document.querySelector("#coolBrowsers");
+  targetList.append(elem);
+
+  // trick to remove the click listener once
+  // the image has been moved into the list
+  elem.onclick = null;
+}
