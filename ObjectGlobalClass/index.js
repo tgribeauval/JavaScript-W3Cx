80593ccf29654c38d5f33288025a9187cadef1b8
Thierry Gribeauval <t.gribeauval11@gmail.com>
2020-03-21 23:00:19 -0400

    String Class, Math Class, Forms and Tables

---
+++ ObjectGlobalClass/index.js
@@ -0,0 +1,20 @@
+var o = {}; // creation of an empty object
+//undefined
+
+var o = new Object(); // same thing as in line 1
+// undefined
+
+o.toString();
+//"[object Object]"
+
+//o.name = 'Michel';
+("Michel");
+
+o.toString();
+//"[object Object]"
+
+var t = [1, 2, 3];
+//undefined
+
+t.toString();
+//"1,2,3"
